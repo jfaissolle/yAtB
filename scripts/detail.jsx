4925739fa3ba2d4ba351f569794bcda6c6f33901
Julien Faissolle <julien.faissolle@gmail.com>
2013-12-31 18:07:05 +0100

    markdown for titles

--- scripts/detail.jsx
+++ scripts/detail.jsx
@@ -2,6 +2,8 @@
 var Components = (function(comps) {
   "use strict";
     
+  var cx = React.addons.classSet;
+  var converter = new Showdown.converter();
   var ReactTransitionGroup = React.addons.TransitionGroup;
 
   var Editable = comps.Editable = React.createClass({
@@ -23,11 +25,12 @@ var Components = (function(comps) {
     },
     render: function() {
       if (!this.state.editing) {
-        return <div onClick={this.handleEdit} className="editable">{this.state.value}</div>;
+        var rawMarkup = converter.makeHtml(this.state.value);
+        return <div onClick={this.handleEdit} className="field editable" dangerouslySetInnerHTML={{__html: rawMarkup}}/>;
       } else {
         return (
           <div>
-            <textarea onChange={this.handleChange} value={this.state.value}></textarea>
+            <textarea rows="4" onChange={this.handleChange} value={this.state.value}></textarea>
             <button onClick={this.handleSave}>Save</button>
             <button onClick={this.handleCancel}>Cancel</button>
           </div>
@@ -59,6 +62,7 @@ var Components = (function(comps) {
     render: function() {
       var self = this;
       var task = this.props.task;
+
       var content = task ? (
         <ReactTransitionGroup transitionName="modal" component={React.DOM.div}>
           <div className="md-content">
@@ -85,7 +89,81 @@ var Components = (function(comps) {
       );
     }
   });
+
+  var TaskCard = comps.TaskCard = React.createClass({
+    getInitialState: function() {
+      return {dragged: false};
+    },
+    handleDragStart: function(e) {
+      this.props.onDrag(e);
+      this.setState({dragged: true});
+    },
+    handleDragEnd: function(e) {
+      e.preventDefault();
+      this.setState({dragged: false});
+      return false;
+    },
+    render: function() {
+      var classes = cx({
+        taskcard: true,
+        dragged: this.state.dragged
+      });
+      var rawMarkup = converter.makeHtml(this.props.task.title);
+      return (
+        <article onDragStart={this.handleDragStart}     
+                 onDragEnd={this.handleDragEnd}
+                 onClick={this.props.onSelect}
+                 draggable="true" className={classes}>
+          <div className="field" dangerouslySetInnerHTML={{__html: rawMarkup}} />
+          <img className="profile-pic"/>
+          <div className="task-icons"></div>
+        </article>
+      );
+    }
+  });
   
+  
+  var CardEditor = comps.CardEditor = React.createClass({
+    getInitialState: function() {
+      return {editing: false};
+    },
+    handleNew: function() {
+      this.setState({editing: true});
+    },
+    handleChange: function(e) {
+      this.setState({tasktext: e.target.value});
+    },
+    handleAdd: function() {
+      this.props.onAdd({title: this.state.tasktext});
+      this.setState({editing: false});
+    },
+    handleCancel: function() {
+      this.setState({editing: false});
+    },
+    componentDidUpdate: function() {
+      if (this.state.editing) {
+        this.refs.textinput.getDOMNode().focus();
+      }
+    },
+    render: function() {
+      var self = this;
+      if (!this.state.editing) {
+        return <button onClick={self.handleNew}>New</button>;
+      } else {
+        return (
+          <div>
+            <article className="taskcard cardeditor">
+              <textarea placeholder="Task Title (Markdown)" ref="textinput" onChange={self.handleChange} />
+            </article>
+            <button onClick={self.handleAdd}>Add</button>
+            <button onClick={self.handleCancel}>Cancel</button>
+          </div>
+        );
+      }
+    }
+  });    
+      
+      
   return comps;
   
 })(Components || {});
